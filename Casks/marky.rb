a8e618d44c48794484640942d9b8ac90b3d4c960
Garrett Graves <grvydev@gmail.com>
2026-04-16 10:36:09 -0400

    init

---
+++ Casks/marky.rb
@@ -0,0 +1,33 @@
+cask "marky" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/GRVYDEV/marky/releases/download/v#{version}/Marky_#{version}_aarch64.tar.gz"
+  name "Marky"
+  desc "Fast markdown viewer with folder support and Cmd+K search"
+  homepage "https://github.com/GRVYDEV/marky"
+
+  depends_on arch: :arm64
+
+  app "Marky.app"
+
+  postflight do
+    wrapper = "#{HOMEBREW_PREFIX}/bin/marky"
+    File.write(wrapper, <<~SH)
+      #!/bin/bash
+      exec /usr/bin/open -a "#{appdir}/Marky.app" --args "$@"
+    SH
+    File.chmod(0755, wrapper)
+  end
+
+  uninstall_postflight do
+    wrapper = "#{HOMEBREW_PREFIX}/bin/marky"
+    File.delete(wrapper) if File.exist?(wrapper)
+  end
+
+  zap trash: [
+    "~/Library/Application Support/dev.marky.app",
+    "~/Library/Caches/dev.marky.app",
+    "~/Library/Preferences/dev.marky.app.plist",
+  ]
+end
